99a991654ef91a2cfa67c1fed5e6253ce598f5d3
Serg Minin <everestdon@ukr.net>
2019-05-07 23:33:07 +0300

    first commit

---
+++ js/modules.js
@@ -0,0 +1,14 @@
+exports.modules = [
+  './js/_intro-mdb-free.js',
+  './js/vendor/jquery.easing.js',
+  './js/vendor/velocity.min.js',
+  './js/vendor/chart.js',
+  './js/vendor/wow.js',
+  './js/dist/scrolling-navbar.js',
+  './js/vendor/waves.js',
+  './js/dist/forms-free.js',
+  './js/vendor/enhanced-modals.js',
+  './js/dist/treeview.js/',
+  './js/vendor/bs-custom-file-input.js'
+  // './js/vendor/addons/datatables.js'
+];
